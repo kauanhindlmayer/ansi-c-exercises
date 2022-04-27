00f5e4c81dbeae322e72e07917c60746a23cc1ea
kauanhindlmayer <kauanindlmayer07@gmail.com>
2022-04-27 10:44:44 -0300

    Exercicio 011

--- listaComplementar3/ex011.c
+++ listaComplementar3/ex011.c
@@ -2,3 +2,18 @@
 Faça um programa que mostre a soma desta serie S= 1/1 + 2/3 + 3/5+ 
 4/7+5/9+..+10/19.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){ 
+
+  float soma = (1/2);
+
+  for (int i = 1; i < 10; i++) {
+    soma += (i / (2 * i));
+  }
+
+  
+	return 0;
+} 
