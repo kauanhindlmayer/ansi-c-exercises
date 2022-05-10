b23030ec565d965be62b5e83b525cc484ea340fb
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-05-09 21:37:34 -0300

    Exercícios

--- listaComplementar3/ex018.c
+++ listaComplementar3/ex018.c
@@ -7,27 +7,25 @@ Faça um programa que peça um número e informe se o número é ou não primo
 
 int main(void){ 
 
-  int teste, num1;
-  
-  printf("Digite um numero:\n");
-  scanf("%i", &num1);
-  fflush(stdin);
-
-  verificaPrimo(num1);
+  verificaPrimo();
   
 	return 0;
 } 
 
-int verificaPrimo(num1) {
-  int verificador = 0;
+void verificaPrimo() {
+  int verificador = 0, numero;
 
-  for(int i = 2; i < num1 / 2; i++) {
-    if(num1 % i == 0) {
+  printf("Digite um numero:\n");
+  scanf("%i", &numero);
+  fflush(stdin);
+
+  for(int i = 2; i < numero; i++) {
+    if(numero % i == 0) {
       verificador = 0;
     } else {
       verificador = 1;
     }
   }
 
-  return (verificador == 1) ? printf("O numero %i e PRIMO!\n", num1) : printf("O numero %i NAO e primo!\n", num1);
+  return (verificador == 1) ? printf("O numero %i e PRIMO!\n", numero) : printf("O numero %i NAO e primo!\n", numero);
 }
