215fbe24ca2cd69fdb6ee8766e9b7c8ed46509d1
kauanhindlmayer <kauanhindlmayer07@gmail.com>
2022-06-10 18:01:07 -0300

    Exercícios

--- lista_complementar_4/ex007.c
+++ lista_complementar_4/ex007.c
@@ -9,8 +9,25 @@
 
 int main(void) {
 
+  int idade[5];
+  float altura[5];
+
+  for(int i = 0; i < 5; i++) {
+    printf("\nEntre com os dados da pessoa %i\n", i +  1);
+    printf("Digite sua idade: ");
+    scanf("%i", &idade[i]);
+    fflush(stdin);
+
+    printf("Digite sua altura: ");
+    scanf("%f", &altura[i]);
+    fflush(stdin);
+  }
+
+  printf("\nOrdem inversa: ");
+
+  for(int i = 4; i >= 0; i--) {
+    printf("\nPessoa %i: %i anos - %.1fm", i + 1, idade[i], altura[i]);
+  }
   
-
-
   return 0;
 }
