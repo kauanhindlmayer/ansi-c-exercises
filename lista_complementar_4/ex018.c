d59e36bb609715a17d0d0e941f4fbec553a4e198
kauanhindlmayer <kauanhindlmayer07@gmail.com>
2022-06-12 11:01:06 -0300

    Exercícios

--- lista_complementar_4/ex018.c
+++ lista_complementar_4/ex018.c
@@ -9,8 +9,28 @@
 
 int main(void) {
 
+  int vetorA[10], vetorB[10], vetorC[20];
+
+  for(int i = 0; i < 10; i++) {
+    printf("Digite o elemento %i: ", i + 1);
+    scanf("%i", &vetorA[i]);
+    fflush(stdin);
+  }
+
+  for(int j = 0; j < 10; j++) {
+    printf("Digite o elemento %i: ", j + 1);
+    scanf("%i", &vetorB[j]);
+    fflush(stdin);
+  }
+
+  printf("\nVetor C: ");
+  for(int k = 0; k < 10; k++) {
+    vetorC[k] = vetorA[k];
+    vetorC[k + 1] = vetorB[k];
+
+    printf("%i, ", vetorC[k]);
+    printf("%i, ", vetorC[k + 1]);
+  }
   
-
-
   return 0;
 }
--- lista_complementar_4/ex019.c
+++ lista_complementar_4/ex019.c
@@ -9,8 +9,39 @@
 
 int main(void) {
 
+  int vetorA[10], vetorB[10], vetorC[10], vetorD[30];
+
+  printf("Entre com os valores do Vetor A\n ");
+  for(int i = 0; i < 10; i++) {
+    printf("Digite o elemento %i: ", i + 1);
+    scanf("%i", &vetorA[i]);
+    fflush(stdin);
+  }
+
+  printf("Entre com os valores do Vetor B\n ");
+  for(int j = 0; j < 10; j++) {
+    printf("Digite o elemento %i: ", j + 1);
+    scanf("%i", &vetorB[j]);
+    fflush(stdin);
+  }
+
+  printf("Entre com os valores do Vetor C\n ");
+  for(int l = 0; l < 10; l++) {
+    printf("Digite o elemento %i: ", l + 1);
+    scanf("%i", &vetorC[l]);
+    fflush(stdin);
+  }
+
+  printf("\nVetor D: ");
+  for(int k = 0; k < 30; k++) {
+    vetorD[k] = vetorA[k];
+    vetorD[k + 1] = vetorB[k];
+    vetorD[k + 2] = vetorC[k];
+
+    printf("%i, ", vetorD[k]);
+    printf("%i, ", vetorD[k + 1]);
+    printf("%i, ", vetorD[k + 2]);
+  }
   
-
-
   return 0;
 }
