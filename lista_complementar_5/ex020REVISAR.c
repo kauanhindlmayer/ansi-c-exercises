08ca274dcb4d6fb2c23bd3df1356ad5ad61bccd6
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-06-20 11:47:29 -0300

    Exercícios

--- lista_complementar_5/ex020REVISAR.c
+++ lista_complementar_5/ex020REVISAR.c
@@ -6,11 +6,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+int calculaDistancia(int longInicial, int longFinal, int latInicial, int latFinal) {
+  return sqrt(sqrt(longFinal - longInicial) + sqrt(latFinal - latInicial));
+}
+
+void recebeCoordenadas(void) {
+
+  int longInicial, longFinal, latInicial, latFinal, res;
+
+  printf("Entre com a Longitude incial: ");
+  scanf("%i", &longInicial);
+  fflush(stdin);
+
+  printf("Entre com a Latitude inicial: ");
+  scanf("%i", &latInicial);
+  fflush(stdin);
+  
+  printf("Entre com a Longitude final: ");
+  scanf("%i", &longFinal);
+  fflush(stdin);
+
+  printf("Entre com a Latitude final: ");
+  scanf("%i", &latFinal);
+  fflush(stdin);
+
+  res = calculaDistancia(longInicial, longFinal, latInicial, latFinal);
+
+  printf("\nDistancia entre as coordenadas: %i", res);
+}
 
 int main(void) {
-
   
-
+  recebeCoordenadas();
 
   return 0;
 }
