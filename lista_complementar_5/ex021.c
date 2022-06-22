ac85543ea48ea344241d15a3e5f3103ce918ed61
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-06-22 09:13:46 -0300

    Exercícios

--- lista_complementar_5/ex021.c
+++ lista_complementar_5/ex021.c
@@ -3,9 +3,6 @@
   base hexadecimal ( 0..9,a,b,c,d,e,f). Este programa deverá ter uma função 
   que é chamada n vezes e devolve o resto da divisão por 16; este valor é 
   armazenado em um vetor que é invertido e então impresso.
-		Ex:    	135 /16
-	          resta 7      8     =>   135 na base 10 = 87 na base 16
-
 */
 
 #include <stdio.h>
